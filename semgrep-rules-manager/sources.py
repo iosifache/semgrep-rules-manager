82afc69bd17b9aa31963747e876126c4aba13f49
George-Andrei Iosif <andrei.iosif@canonical.com>
2023-07-27 18:05:58 +0300

    Ensures all YAML files are valid

--- semgrep-rules-manager/sources.py
+++ semgrep-rules-manager/sources.py
@@ -1,11 +1,66 @@
 import os
+import re
 import shutil
 import typing
+from abc import abstractmethod
 from dataclasses import dataclass
 
 import git
 import yaml
 from exception import SemgrepRulesManagerException
+
+
+class Preprocessor:
+    IDENTIFIER = None
+    location: str
+
+    def __init__(self, location: str) -> None:
+        self.location = location
+
+    @abstractmethod
+    def preprocess(self) -> None:
+        pass
+
+
+class IDStandardizationPreprocessor(Preprocessor):
+    IDENTIFIER = "id-standardization"
+
+    def __init__(self, location: str):
+        super().__init__(location)
+
+    def preprocess(self) -> None:
+        for file in self._get_all_yaml_files():
+            self.process_content(file)
+
+    def process_content(self, filename: str) -> None:
+        with open(filename, "r+") as file:
+            content = file.read()
+
+            new_content = re.sub(
+                r"\n- id: \".*\/(.*)\"\n", r"\n- id: \1\n", content
+            )
+
+            file.seek(0)
+            file.truncate()
+            file.write(new_content)
+
+    def _get_all_yaml_files(self) -> typing.Generator[str, None, None]:
+        for root, _, files in os.walk(self.location):
+            for file in files:
+                if file.endswith(".yaml") or file.endswith(".yml"):
+                    yield os.path.join(root, file)
+
+
+class PreprocessorsFactory:
+    known_preprocessors = [IDStandardizationPreprocessor]
+
+    @staticmethod
+    def create(identifier: str, location: str) -> Preprocessor:
+        for preprocessor in PreprocessorsFactory.known_preprocessors:
+            if preprocessor.IDENTIFIER == identifier:
+                return preprocessor(location)
+
+        raise PreprocessorNotFoundException()
 
 
 @dataclass
@@ -16,6 +71,8 @@ class Source:
     repo_brach: str
     author: str
     license: str
+    preprocessors_ids: typing.List[str]
+    ignored: typing.List[str]
     location: str
     is_downloaded: bool = False
     local_commit: str = None
@@ -41,6 +98,26 @@ class Source:
 
     def download(self) -> None:
         git.Repo.clone_from(self.repo_url, self.location)
+
+        self._preprocess()
+        self._remove_ignored()
+
+    def _preprocess(self) -> None:
+        for preprocessor_id in self.preprocessors_ids:
+            preprocessor = PreprocessorsFactory.create(
+                preprocessor_id, self.location
+            )
+
+            preprocessor.preprocess()
+
+    def _remove_ignored(self) -> None:
+        for current_ignored in self.ignored:
+            path = os.path.join(self.location, current_ignored)
+
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
 
     def update(self) -> None:
         repo = git.Repo(self.location)
@@ -75,6 +152,8 @@ def read_sources(
                 details["repository_branch"],
                 details["author"],
                 details["license"],
+                details.get("preprocessors", []),
+                details.get("ignored", []),
                 location,
             )
             yield_once = True
@@ -84,6 +163,10 @@ def read_sources(
 
         if identifier and not yield_once:
             raise SourceNotFoundException()
+
+
+class PreprocessorNotFoundException(SemgrepRulesManagerException):
+    """A preprocessor specified in sources YAML is not implemented."""
 
 
 class SourceNotFoundException(SemgrepRulesManagerException):
