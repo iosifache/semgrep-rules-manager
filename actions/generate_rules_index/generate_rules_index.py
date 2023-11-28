47841a31d273bd3fcddbc793e73d7e61b701a744
George-Andrei Iosif <andrei.iosif@canonical.com>
2023-11-28 14:28:26 +0200

    Changes the index to be JSON

--- actions/generate_rules_index/generate_rules_index.py
+++ actions/generate_rules_index/generate_rules_index.py
@@ -1,58 +1,48 @@
 #!/usr/bin/env python3
 
-import tabulate
+import json
 import typing
 
 
 from semgrep_rules_manager.sources import read_sources, Source
 from semgrep_rules_manager.helpers import translate_lang_id_to_name
 
-INDEX_NAME = "RULES_INDEX.md"
+INDEX_NAME = "semgrep_rules_manager/data/index.json"
 
 
-def create_table_with_rules(sources: typing.Iterable[Source]) -> str:
-    table = []
+def create_index_from_rules(sources: typing.Iterable[Source]) -> list:
+    index = []
     for source in sources:
         for file in source.get_rule_files():
             for rule in file.rules:
-                table.append(
-                    [
-                        f"`{rule.identifier}`",
-                        rule.message,
-                        f"`{source.identifier}`",
-                        f"`{rule.severity.name}`",
-                        ", ".join(
+                index.append(
+                    {
+                        "identifier": f"{rule.identifier}",
+                        "message": rule.message,
+                        "parent_source": f"{source.identifier}",
+                        "severity": f"`{rule.severity.name}`",
+                        "languages": ", ".join(
                             [
                                 translate_lang_id_to_name(lang)
                                 for lang in rule.languages
                             ]
                         ),
-                    ]
+                    }
                 )
 
-    return tabulate.tabulate(
-        table,
-        [
-            "Identifier",
-            "Message",
-            "Parent source",
-            "Severity",
-            "Languages",
-        ],
-        tablefmt="github",
-    )
+    return index
 
 
-def dump_to_index_file(table: str) -> None:
+def dump_to_index_file(table: list) -> None:
     with open(INDEX_NAME, "w", encoding="utf-8") as rules_index_file:
-        rules_index_file.write(table)
+        rules_index_file.write(json.dumps(table))
 
 
 def main() -> None:
     sources = read_sources("/tmp")
-    table = create_table_with_rules(sources)
+    index = create_index_from_rules(sources)
 
-    dump_to_index_file(table)
+    dump_to_index_file(index)
 
 
 if __name__ == "__main__":
